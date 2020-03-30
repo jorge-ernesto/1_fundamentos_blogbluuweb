93791dd424a17f9c1a9bc72063025cd52b721388
Jorge Ernesto <jorge.cywdt@gmail.com>
2020-03-30 16:39:18 -0500

    Modificacion de views/notas/alerts.blade.php

--- resources/views/notas/alerts.blade.php
+++ resources/views/notas/alerts.blade.php
@@ -1,19 +1,21 @@
-<!-- Alertas -->
+{{-- Alertas --}}
 <div class="mt-4">
     @if( session('mensaje') )
         <div class="alert alert-success">{{ session('mensaje') }}</div>
     @endif
 
+    @if( session('mensaje_eliminado') )
+        <div class="alert alert-danger">{{ session('mensaje_eliminado') }}</div>
+    @endif
+    
+    {{-- Errores --}}
     @error('nombre')
         <div class="alert alert-danger">El nombre es requerido</div>        
     @enderror
 
     @if( $errors->has('descripcion') )
         <div class="alert alert-danger">La descripción es requerida</div>
-    @endif
-
-    @if( session('mensaje_eliminado') )
-        <div class="alert alert-danger">{{ session('mensaje_eliminado') }}</div>
-    @endif
-</div>    
-<!-- Fin Alertas -->
+    @endif    
+    {{-- Fin Errores --}}
+</div>
+{{-- Fin Alertas --}}
