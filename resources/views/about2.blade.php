6bf959d6e3a32403397009945e9d84821afcd30b
Jorge Ernesto <jorge.cywdt@gmail.com>
2020-10-01 19:15:37 -0500

    Arreglamos routes/web.php

---
+++ resources/views/about2.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('seccion-main')
+    <div class="container-fluid">
+        <h1 class="mt-4">{{$titulo}}</h1>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,24 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/* Ruta por defecto */
+//Ruta por defecto
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 Route::get('/', 'NotasController@index');
 
-/* Notas */
-Route::get('/notas/{id?}'       , 'NotasController@index')   ->name('notas.index'); //Acá esta index, create y show
-Route::post('/notas/store'      , 'NotasController@store')   ->name('notas.store');
-Route::get('/notas/{id}/edit'   , 'NotasController@edit')    ->name('notas.edit');
-Route::put('/notas/{id}'        , 'NotasController@update' ) ->name('notas.update');
-Route::get('/notas/{id}/destroy', 'NotasController@destroy' )->name('notas.destroy');
+//RUTAS DE LA APLICACION        
+    //Rutas de paginas
+    Route::get('/welcome'           , 'PagesController@welcome') ->name('welcome.index');
+    Route::get('/blog'              , 'PagesController@blog')    ->name('blog.index');
+    Route::get('/about'             , 'PagesController@about')   ->name('about.index');
+    Route::get('/nosotros/{nombre?}', 'PagesController@nosotros')->name('nosotros.index');   
+    Route::get('/fotos/{id?}'       , 'PagesController@fotos')->where('id', '[0-9]+'); //Solo aceptara números en el parametro
+    Route::view('/about2'           , 'about2', ['titulo' => "About2"]); 
 
-Route::get('/welcome'           , 'PagesController@welcome') ->name('welcome.index');
-Route::get('/blog'              , 'PagesController@blog')    ->name('blog.index');
-Route::get('/about'             , 'PagesController@about')   ->name('about.index');
-Route::get('/nosotros/{nombre?}', 'PagesController@nosotros')->name('nosotros.index');
-
-/* Pruebas */
-Route::get('/fotos/{id?}', 'PagesController@fotos')->where('id', '[0-9]+'); //Solo aceptara números en el parametro
-Route::view('/about2'    , 'about', ['wea' => "KKK"]);
+    //Rutas de notas
+    Route::get('/notas/{id?}'       , 'NotasController@index')   ->name('notas.index'); //Acá esta index, create y show
+    Route::post('/notas/store'      , 'NotasController@store')   ->name('notas.store');
+    Route::get('/notas/{id}/edit'   , 'NotasController@edit')    ->name('notas.edit');
+    Route::put('/notas/{id}'        , 'NotasController@update' ) ->name('notas.update');
+    Route::get('/notas/{id}/destroy', 'NotasController@destroy' )->name('notas.destroy');    
